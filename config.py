e95f8d812dd900c3472572d7814555cdbedfb584
STARK 69 <166913769+Annidon420@users.noreply.github.com>
2026-02-18 21:18:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,5 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
 ADMIN_KEY = os.getenv("ADMIN_KEY")
-
-FORCE_CHANNELS = ["@yourchannelusername"]
